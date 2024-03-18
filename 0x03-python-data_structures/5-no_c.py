e8333b676929786b72318a3325cddd4b7e86b370
Ahmed Hassan <ah01157548281@gmail.com>
2024-03-18 12:29:55 -0700

    s

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     ret_str = ""
     for i in range(len(my_string)):
-        if (my_string[i] != 'c' and != my_string[i] != 'C');
+        if (my_string[i] != 'c' and my_string[i] != 'C');
         ret_str += my_string[i]
     return ret_str
