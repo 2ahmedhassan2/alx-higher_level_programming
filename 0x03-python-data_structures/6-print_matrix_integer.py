f3386cd198e89f2380a4feba048dc1d1533be240
Ahmed Hassan <ah01157548281@gmail.com>
2024-03-18 12:45:47 -0700

    s

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for v in matrix:
+        if len(v) == 0:
+            print()
+        for i in range(len(v)):
+            print("{:d}".format(v[i]),
+                  end="\n" if i is len(v) -1 else " ")
