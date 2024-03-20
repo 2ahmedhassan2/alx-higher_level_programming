9438b2d49fb34947c948584b3e546f3029148c29
Ahmed Hassan <ah01157548281@gmail.com>
2024-03-20 11:39:28 -0700

    s

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return {i: [x ** 2 for x in row] for i, row in enumerate(a_dictionary)}
+    for n in sorted(a_dictionary.keys()):
+        print("{}: {}".format(n, a_dictionary[n]))
